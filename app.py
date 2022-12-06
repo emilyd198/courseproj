bbdc02a6440253e9b0f28a692ec8542d36b33388
emilyd198 <84804090+emilyd198@users.noreply.github.com>
2022-12-06 16:34:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 if 'WEBSITE_HOSTNAME' not in os.environ:
     # local development, where we'll use environment variables
     print("Loading config.development and environment variables from .env file.")
-    app.config.from_object('azureproject.development')
+    app.config.from_object('azureproject/development.py')
 else:
     # production
     print("Loading config.production.")
-    app.config.from_object('azureproject.production')
+    app.config.from_object('azureproject/production.py')
 
 app.config.update(
     SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
